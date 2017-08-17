bcecd2077d75aab156b5d5f74d8fc46d4b5570ef
danielemanuel <daniel.costea@vkernel.ro>
2017-08-17 11:48:02 +0100

    * Set up the project * Create the react components * install yutube serach api

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyB5SMeUMehuumf8ger6TWNg54fz4SR2wAw';
+
+const App = () => {
+  return <div>Hi !</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
