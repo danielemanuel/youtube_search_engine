0441501d8e52a02b6ed2a4ab4c2fde04b4894be5
danielemanuel <daniel.costea@vkernel.ro>
2017-08-17 13:54:18 +0100

    * getting response from the yutube API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyB5SMeUMehuumf8ger6TWNg54fz4SR2wAw';
 
+YTSearch({ key: API_KEY, term: 'sufboards' }, function(data) {
+  console.log(data)
+});
+
 const App = () => {
-  return <div>Hi !</div>
+  return (<div>
+            <SearchBar />
+          </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
